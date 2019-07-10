4e18495feb97ab58f4d416f7bee91eb3553e5b52
Fabricio <fabricio.matos@bridge.ufsc.br>
2019-07-10 10:37:13 -0300

    modificando estrutura de verificacao do resultado

--- app/src/main/java/com/fabriciomatos/pedrapapeltesoura/controller/ControllerJogada.java
+++ app/src/main/java/com/fabriciomatos/pedrapapeltesoura/controller/ControllerJogada.java
@@ -14,17 +14,9 @@ public class ControllerJogada {
         int jogadaPC = random.nextInt(3);
         jogada = new Jogada(jogadaHumano,jogadaPC);
 
-        if(jogadaHumano == 0 && jogadaPC == 1){
+        if((jogadaHumano == 0 && jogadaPC == 1) || (jogadaHumano == 1 && jogadaPC == 2) || (jogadaHumano == 2 && jogadaPC == 0)){
             jogada.setDescricaoJogada("Você perdeu :/");
-        }else if(jogadaHumano == 0 && jogadaPC == 2){
-            jogada.setDescricaoJogada("Você ganhou |o|");
-        }else if(jogadaHumano == 1 && jogadaPC == 0){
-            jogada.setDescricaoJogada("Você ganhou |o|");
-        }else if(jogadaHumano == 1 && jogadaPC == 2){
-            jogada.setDescricaoJogada("Você perdeu :/");
-        }else if(jogadaHumano == 2 && jogadaPC == 0) {
-            jogada.setDescricaoJogada("Você perdeu :/");
-        }else if(jogadaHumano == 2 && jogadaPC == 1){
+        }else if((jogadaHumano == 0 && jogadaPC == 2) || (jogadaHumano == 1 && jogadaPC == 0) || (jogadaHumano == 2 && jogadaPC == 1)){
             jogada.setDescricaoJogada("Você ganhou |o|");
         }else
             jogada.setDescricaoJogada("Empate! Tente novamente!");
